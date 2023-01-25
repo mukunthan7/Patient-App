35743f8a177646c7af697a5d9ce35035da8a5c68
mukunthan7 <vijimurugesan7@gmail.com>
2023-01-25 20:47:48 +0530

    Updated in Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import SidebarPatient from '../components/SidebarPatient';
 
 const Home = () => {
-  return (
-    <div>Home</div>
+    return (
+    <div className='min-h-screen bg-pink-200 items-center'>
+      <SidebarPatient />
+        <div className='flex justify-center'>
+            <h1>patient Home</h1>
+        </div>
+    </div>
   )
 }
 
